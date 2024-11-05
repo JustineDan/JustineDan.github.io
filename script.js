c7afc9312480b2cdfc0fe953224aa3458d7bb1ce
Justine Daniel Moralde <justine.daniel.moralde@gmail.com>
2024-11-05 21:43:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function startGuide() {
+    // Hide the intro section
+    const introSection = document.getElementById('intro');
+    introSection.style.display = 'none';
+
+    // Show the main content
+    const mainContent = document.getElementById('main-content');
+    mainContent.style.display = 'block';
+}
